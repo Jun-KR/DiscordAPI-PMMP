1dd4571f1246ef168552e4fb208961dba2efda6a
Jun-KR <jundevkr@gmail.com>
2021-07-24 18:37:11 +0900

    Change "sendWebhook" to asynchronous

--- src/DiscordAPI/manager/WebhookManager.php
+++ src/DiscordAPI/manager/WebhookManager.php
@@ -5,18 +5,32 @@ declare(strict_types=1);
 namespace DiscordAPI\manager;
 
 use DiscordAPI\webhook\Webhook;
+use pocketmine\scheduler\BulkCurlTask;
+use pocketmine\Server;
 
-class WebhookManager {
+class WebhookManager{
 
-    public static function sendWebhook(Webhook $webhook, string $url) : void {
-        $handle = curl_init($url);
-        curl_setopt($handle, CURLOPT_POSTFIELDS, json_encode($webhook));
-        curl_setopt($handle, CURLOPT_POST, true);
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYHOST, false);
-        curl_setopt($handle, CURLOPT_SSL_VERIFYPEER, false);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, ["Content-Type: application/json"]);
-        curl_exec($handle);
-        curl_close($handle);
+    public static function sendWebhook(Webhook $webhook, string $url) : void{
+        $server = Server::getInstance();
+
+        $server->getAsyncPool()->submitTask(new class($url, $webhook) extends BulkCurlTask{
+            public function __construct($url, $webhook){
+                parent::__construct([
+                    [
+                        "page" => $url,
+                        "extraOpts" => [
+                            CURLOPT_HTTPHEADER => [
+                                "Content-Type: application/json"
+                            ],
+                            CURLOPT_POSTFIELDS => json_encode($webhook),
+                            CURLOPT_POST => true,
+                            CURLOPT_RETURNTRANSFER => true,
+                            CURLOPT_SSL_VERIFYHOST => false,
+                            CURLOPT_SSL_VERIFYPEER => false,
+                        ]
+                    ]
+                ]);
+            }
+        });
     }
 }
